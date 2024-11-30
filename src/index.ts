16f3ec61f661a37857ca7b8384f88917073137bd
chrislo <chris@soychristian.com>
2024-11-30 15:30:32 -0500

    root path response has been change

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 
 // GET /users/packages?id=101
 app.get("/", (request, response) => {
-	response.send("Hello, world!");
+	response.send("Hello, world");
 });
 
 app.listen(3000);
